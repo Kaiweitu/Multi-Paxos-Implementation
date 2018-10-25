2f0f4891f5a26c067dcf7f20a087908601372203
kaiweitu@umich.edu <kaiweitu@umich.edu>
2018-10-24 23:41:31 -0400

    Merge Util

--- Server/Util.h
+++ Server/Util.h
@@ -3,14 +3,42 @@
 using namespace std;
 
 
+long int getCurrentTime();
 
+struct acceptMsg {
+    int slot;
+    int seq;
+    int client_ID;
+    int port;
+    // Network Byte Order
+    unsigned long client_IP;
+    string command;
 
+    static string serialize(acceptedMsg &msg) {
+        ostringstream oss;
+        oss << slot << ' ' << client_IP << ' ' << port << ' ' << seq
+        << client_ID << ':' << command;
+        
+        return oss.str();
+    }
 
+    static acceptMsg deserialize(string &msg) {
+        acceptMsg msg;
+        istringstream iss(msg);
+        
+        int index = msg.find_first_of(':');
+        string cmd(msg.begin() + index + 1, msg.end());
+        msg.command = std::move(cmd);
+        iss >> msg.slot >> msg.client_IP >> msg.port >> msg.seq >> msg.client_ID;
 
+        return std::move(msg);
+    }
+}
 
-
-
-
+struct learnerHeartBeatMsg {
+    int first_unchosen_index;
+    int server_id;
+}
 
 
 
