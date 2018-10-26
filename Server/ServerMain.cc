438098e95e6fd7e6f5bf40cff0293fbaf4e1bed3
kaiweitu@umich.edu <kaiweitu@umich.edu>
2018-10-25 22:06:33 -0400

    Add SeverMain.cc

---
+++ Server/ServerMain.cc
@@ -0,0 +1,31 @@
+#include "Header.h"
+using namespace std;
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        exit(1);
+    } 
+    int sId = atoi(argv[1]);
+
+    const string config_file = "service.config";
+    ifstream ifs;
+    ifs.open(config_file);
+    
+    int replica_num;
+    ifs >> replica_num;
+    vector<string> hosts;
+    vector<int> ports;
+    hosts.resize(replica_num);
+    ports.resize(replica_num);
+    
+    for (int i = 0; i < replica_num; i++) {
+        string s_ip;
+        int port;
+        int id;
+        ifs >> id >> s_ip >> port;
+        hosts[id] = s_ip;
+        ports[id] = port;
+    }
+    Server server(ports[sId], sId,  hosts[sId],hosts, ports);
+    server.start();
+}
