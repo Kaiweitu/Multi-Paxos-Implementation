dd71e800eeb4f1414003c4ba3d9c43d798705f7d
kaiweitu@umich.edu <kaiweitu@umich.edu>
2018-10-25 22:19:50 -0400

    Change the Client.cc

--- Client/Client.cc
+++ Client/Client.cc
@@ -1,6 +1,6 @@
 #include "ClientStub.h"
 
 int main() {
-    ClientStub ct(0, 8000, "localhost", "service.config");
+    ClientStub ct(0, 8000, "127.0.0.1", "service.config");
     ct.sendMessage("test");
 }
